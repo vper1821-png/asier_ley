0cc8fc66ab4f11dcf4a14f6a8d0f6680c56fafd9
vper1821-png <vper1821-png@users.noreply.github.com>
2026-08-14 23:43:24 -0400

    fix config .php

--- backend/config.php
+++ backend/config.php
@@ -18,7 +18,8 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json; charset=utf-8');
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+
     http_response_code(204);
     exit;
 }
