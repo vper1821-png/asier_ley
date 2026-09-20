44cc62a213d9d235f98a7f20d43d6e9514fec5d6
vper1821-png <vper1821-png@users.noreply.github.com>
2026-09-20 15:44:23 -0300

    limpieza

--- backend/test_user.php
+++
@@ -1,6 +0,0 @@
-<?php
-require '/var/www/html/config.php';
-require '/var/www/html/Database.php';
-$db = Database::getInstance();
-$u = $db->findOne('users', ['email' => 'test@test.com']);
-var_export($u);
